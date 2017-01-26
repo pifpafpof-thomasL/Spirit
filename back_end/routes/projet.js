fce1506e02e64aecd7fdd1e65e421487b5faa952
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-01-26 15:50:05 +0100

    added update rest and corrected error code returned

--- back_end/routes/projet.js
+++ back_end/routes/projet.js
@@ -3,40 +3,46 @@
 const express = require('express')
 const app = express()
 
-const inspect = require('util').inspect
+//const inspect = require('util').inspect
 
 module.exports = (db, viewpath = 'projets') => {
 
    app.get('/', (req, res) => {
       db.Projet.findAll({})
-      .then((Projets) => res.json(Projets))
+      .then((projets) => res.status(200).json(projets))
+      .catch( e => res.status(404).send('db access error') )
    })
 
    app.get('/:id', (req, res) => {
       db.Projet.find({ where: { id_projet: req.params.id } })
-      .then((Projet) => Projet ?
-         res.json(Projet) :
-         res.status(404).json({ error: "unknown Projet" })
+      .then((projet) => projet ?
+         res.status(200).json(projet) :
+         res.status(404).send('Not Found')
       )
    })
 
    app.delete('/:id', (req, res) => {
       db.Projet.findById(req.params.id)
       .then( (projet) => projet.destroy() )
-      .then( () => res.status(204).send() )
-      .catch(e => res.status(404).send('no such user'))
+      .then( () => res.status(200).send('delete ok') )
+      .catch(e => res.status(404).send('error'))
    })
 
    app.post('/', (req, res) => {
-      console.log(inspect(req.body))
       db.Projet.create(req.body)
-      .then( () => res.status(204).send('ok') )
-      .catch( e => {
-         console.log(inspect(e))
-         res.status(404).send('error')
+      .then( projet => {
+         const id_projet = projet.dataValues.id_projet
+         res.status(201).append('Location', '/' + id_projet).send('created ok')
       })
+      .catch( e => res.status(404).send('error') )
    })
 
+   app.put('/:id', (req, res) => {
+      db.Projet.findById(req.params.id)
+      .then( projet => projet.update(req.body) )
+      .then( res.status(204).send('update ok') )
+      .catch( e => res.status(404).send('error') )
+   })
 
    return app
 }
