a26845a18743bc040151a565c26c8f6c66d88ad4
François Raynal <raynal0francois@gmail.com>
2017-02-01 09:56:56 +0100

    fixed tache post route

--- back_end/routes/tache.js
+++ back_end/routes/tache.js
@@ -6,7 +6,7 @@ module.exports = (db, viewpath = 'Tache') => {
   //list all tasks
   app.get('/', (req, res) => {
     db.Tache.findAll()
-    .then(task => res.json(task))
+    .then(task => res.status(200).json(task))
     .catch(error => res.sendStatus(404))
   })
 
@@ -16,20 +16,22 @@ module.exports = (db, viewpath = 'Tache') => {
     .then((task) => task ?
       res.status(200).json(task) :
       res.sendStatus(404))
-    .catch(error => console.log(error))
+    .catch(error => res.send('An error has occured'))
   })
 
   //create a new task
   app.post('/', (req, res) => {
     db.Tache.create(req.body)
-    //.then(tache => res.status(201).send(`${tache} créée`)).then(tache => res.location())
     .then(task => {
-      // TODO : redirect à paufiner
-      //let id_tache = task.dataValues
-      //console.log(id_tache)
-      res.redirect(201, `/tache/${req.body.id_Tache}`)
+      res.location(`/tache/${req.body.id_Tache}`).sendStatus(201)
     })
-    .catch(error => res.json(e))
+    .catch(error => {
+      // Works but does 2 requests to the db to verify
+      if (db.Tache.findById(req.body.id_Tache)) {
+        return res.status(409).send('This task already exists')
+      }
+      res.sendStatus(404)
+    })
   })
 
   // delete a task
