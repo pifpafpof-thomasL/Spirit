5e800a576bcfd7892e6c43755eec5b3518028493
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-02-03 22:18:47 +0100

    models/Imputations: corrected autoImplement spelling

--- back_end/lib/models/Imputation.js
+++ back_end/lib/models/Imputation.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
          type: DataTypes.INTEGER(10),
          allowNull: false,
          primaryKey: true,
-         autoincrement: true
+         autoIncrement: true
       },
       id_Consultant: {
          type: DataTypes.INTEGER(10),
