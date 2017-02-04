9a035092208516238776e4ce6bc7fcb03a05b73d
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-02-04 18:40:34 +0100

    added tests for affectations routes

--- back_end/test/affectation-routes-test.js
+++ back_end/test/affectation-routes-test.js
@@ -6,24 +6,28 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const expect = chai.expect
 
-const URL = 'http://localhost:3000/technos'
+const URL = 'http://localhost:3000/affectations'
 
 chai.use(chaiHttp)
 
 // TEST DATAS
-const techno_data_json = {
-   "Nom":"technoName",
+const affectation_data_json = {
+   "id_Consultant":2,
+   "id_Projet":45,
+   "Pourcentage": 50
 }
 
-const techno2_data_json = {
-   "Nom":"AutreTechnoNom",
+const affectation2_data_json = {
+   "id_Consultant":2,
+   "id_Projet":45,
+   "Pourcentage": 99
 }
 
 
-describe('technos CRUD routes', function() {
+describe('affectations CRUD routes', function() {
    // global var initialized in the add test
    // and used in update and delete tests
-   let location_new_techno= ''
+   let location_new_affectation= ''
 
    it('get collection silently', function(done) {
       chai.request(URL)
@@ -37,56 +41,56 @@ describe('technos CRUD routes', function() {
    it('add', function(done) {
       chai.request(URL)
       .post('/')
-      .send(techno_data_json)
+      .send(affectation_data_json)
       .end(function (err, res) {
          expect(res.headers).to.have.property('location')
-         expect(res.headers.location).to.match(/^\/technos\/[0-9]+$/)
+         expect(res.headers.location).to.match(/^\/affectations\/[0-9]+$/)
          expect(res).to.have.status(201)
-         location_new_techno = res.headers.location.slice(8)
+         location_new_affectation = res.headers.location.slice(13)
          done()
       })
    })
 
-   it('get the new techno', function(done) {
+   it('get the new affectation', function(done) {
       chai.request(URL)
-      .get(location_new_techno)
+      .get(location_new_affectation)
       .end(function(err, res) {
          expect(res).to.have.status(200)
-         expect(res.body).to.include(techno_data_json)
+         expect(res.body).to.include(affectation_data_json)
          done()
       })
    })
 
    it('update', function(done) {
       chai.request(URL)
-      .put(location_new_techno)
-      .send(techno2_data_json)
+      .put(location_new_affectation)
+      .send(affectation2_data_json)
       .end(function(err, res) {
          expect(res).to.have.status(204)
          done()
       })
    })
 
-   it('fails updating non existing techno', function(done) {
+   it('fails updating non existing affectation', function(done) {
       chai.request(URL)
       .put('/wrongId')
-      .send(techno2_data_json)
+      .send(affectation2_data_json)
       .end(function(err, res) {
          expect(res).to.have.status(404)
          done()
       })
    })
 
-   it('delete an existing techno', function(done) {
+   it('delete an existing affectation', function(done) {
       chai.request(URL)
-      .delete(location_new_techno)
+      .delete(location_new_affectation)
       .end(function(err, res) {
          expect(res).to.have.status(204)
          done()
       })
    })
 
-   it('fails when delete non existing techno', function(done) {
+   it('fails when delete non existing affectation', function(done) {
       chai.request(URL)
       .delete('/notAnId')
       .end(function(err, res) {
